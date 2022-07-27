261a5cfa9505171b21313568672888ec9eec229d
Krafty Kactus <tkk13909@gmail.com>
2022-07-27 17:07:42 -0600

    updated

--- install.sh
+++ install.sh
@@ -1,13 +1,2 @@
-sudo cp ./home/ /
+sudo cp ./home/user/* ~/ -r
 cp ./wallpapers/ ~/Pictures -r
-
-dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-var Desktops = desktops();                                                                                                                       
-for (i=0;i<Desktops.length;i++) {
-        d = Desktops[i];
-        d.wallpaperPlugin = "org.kde.image";
-        d.currentConfigGroup = Array("Wallpaper",
-                                    "org.kde.image",
-                                    "General");
-        d.writeConfig("Image", "file:///~/Pictures/wallpapers/1138740.png");
-}'
